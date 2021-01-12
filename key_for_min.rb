fe7d704249a90f3e967c5185879a4fc630b41f25
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-12 15:30:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,8 @@
 def key_for_min_value(name_hash)
   smallest_number = 999999999
   smallest_name = ' '
-  if 
+  if name_hash != {}
+    
   name_hash.each do |name, value|
     if value < smallest_number
       smallest_number = value
