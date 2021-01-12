c368dae81716bb3aaca400d1cd1a47dcb1a66e87
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-12 15:04:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,8 @@
 def key_for_min_value(name_hash)
  smallest_number = name_hash[:name]
  smallest_name = ''
- 
-  puts smallest_number
-  smallest_name
+ puts smallest_number
+ puts smallest_name
 end
 
 hash = {:blake => 500, :ashley => 2, :adam => 1}
