9b8d31af6b927cee5f0e2ed7334145dcb49a142c
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-12 15:38:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash.nil
+  if name_hash.nil?
     puts "Nil"
     return name_hash
   else
