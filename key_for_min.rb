fdb62422a789b775b9851bfe019c85c3aeed0350
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-12 15:03:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+ smallest_number = name_hash[:name]
+ smallest_name = ''
+ name_hash.each do |name, number|
+   if number < smallest_number
+     smallest_number = number
+     smallest_name = name
+   end
+  end 
+  puts smallest_number
+  smallest_name
 end
+
+hash = {:blake => 500, :ashley => 2, :adam => 1}
+key_for_min_value(hash)
