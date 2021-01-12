847f78315a2bde298a13600e2847e6d305450122
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-12 15:35:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,23 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest_number = 999999999
-  smallest_name = ''
-  if name_hash != {}
-    name_hash.each do |name, value|
-    if value < smallest_number
-      smallest_number = value
-      smallest_name = name
-    end
-   end
-  else smallest_name = nil
-  end 
-  puts smallest_number
-  puts smallest_name
-  smallest_name
+  if name_hash = {}
+    puts NIL
+  else
+    puts "Not nil"
+  end
 end
 
 
 hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 key_for_min_value(hash)
+
+nil_hash = {}
+
+key_for_min_value(nil_hash)
+
